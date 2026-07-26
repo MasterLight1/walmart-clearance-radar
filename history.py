12db859e54b5b70d7aab96499bda0000574f4ed8
MasterLight1 <agustin.morillo.s@hotmail.com>
2026-07-26 10:19:15 -0400

    Implement deal history management functions
    Added functions to load and save deal history in JSON format.

--- history.py
+++ history.py
@@ -1,1 +1,39 @@
+import json
+import os
+from datetime import datetime
 
+
+ARCHIVO = "deal_history.json"
+
+
+def cargar_historial():
+
+    if not os.path.exists(ARCHIVO):
+        return {}
+
+    with open(ARCHIVO, "r") as archivo:
+        return json.load(archivo)
+
+
+
+def ya_enviado(titulo):
+
+    historial = cargar_historial()
+
+    return titulo in historial
+
+
+
+def guardar_deal(deal):
+
+    historial = cargar_historial()
+
+    historial[deal.titulo] = {
+        "tienda": deal.tienda,
+        "precio": deal.precio_actual,
+        "fecha": str(datetime.now())
+    }
+
+
+    with open(ARCHIVO, "w") as archivo:
+        json.dump(historial, archivo, indent=4)

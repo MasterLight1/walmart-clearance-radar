62e2299462a894ff346c2a6e588bffdbfe0eaefb
MasterLight1 <agustin.morillo.s@hotmail.com>
2026-07-26 11:00:24 -0400

    Add keywords to QUERIES list in amazon_queries.py
    Added keywords for Amazon search queries.

--- amazon_queries.py
+++ amazon_queries.py
@@ -1,1 +1,17 @@
+"""
+Palabras clave que el radar buscará en Amazon.
+"""
 
+
+QUERIES = [
+    "clearance",
+    "deal",
+    "sale",
+    "discount",
+    "Ninja",
+    "Nike",
+    "Adidas",
+    "Levi's",
+    "DeWalt",
+    "Keurig"
+]

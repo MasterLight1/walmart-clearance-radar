deae6cb11d8c0249bd3c3ea580b99b190f94afba
MasterLight1 <agustin.morillo.s@hotmail.com>
2026-07-26 10:35:29 -0400

    Add docstring and define obtener_ofertas_amazon function
    Add docstring for external offer sources and define a function for Amazon offers.

--- sources.py
+++ sources.py
@@ -1,1 +1,15 @@
+"""
+Fuentes externas de ofertas.
 
+Aquí conectaremos:
+- Amazon
+- Walmart
+- Target
+- etc.
+"""
+
+
+def obtener_ofertas_amazon():
+
+    # Próximamente conectaremos la fuente real
+    return []
